47bf4b30bf7b1d51cf47454b451ed93f8a5b5ce5
YuDe <davisfreeman1015@gmail.com>
2016-09-29 15:54:55 +0800

    The basic appearance of the timeline is finished.

--- components/eventline-c.js
+++ components/eventline-c.js
@@ -8,10 +8,21 @@ var EventLine = function () {
 	function EventLine() {
 		_classCallCheck(this, EventLine);
 
+		this.pad = d3.select('#TimeLineBoard');
+
+		this.evtsData = [];
 		this.evtlineG = null;
+		this.evtLineY = null; // The Y position of the event line.
 		this.eventNodes = null;
 
-		this.line = null;
+		// this.line = null;
+		this.evtInfoBoard = null;
+
+		// The date weight for putting date in position.
+		this.timeWeight = null;
+		this.timeSpace = 5;
+
+		this.textPointer = null;
 
 		// Time scale
 		this.timeScale = null;
@@ -21,8 +32,12 @@ var EventLine = function () {
 
 	_createClass(EventLine, [{
 		key: 'initialize',
-		value: function initialize() {
-			this.evtlineG = d3.select('svg').append('g').attr('id', 'EvtLine');
+		value: function initialize(svgWidth) {
+			this.evtlineG =
+			// d3.select('svg')
+			this.pad.attr('width', svgWidth).append('g').attr('id', 'EvtLine');
+
+			this.evtInfoBoard = d3.select('#TimeLineInfo').style('width', svgWidth + 'px');
 
 			return this;
 		}
@@ -32,32 +47,148 @@ var EventLine = function () {
 			var _this = this;
 
 			d3.csv(path).row(function (d, i) {
-				if (i !== 0) return d;
+				return d;
 			}).get(function (error, rows) {
 
-				var colorScale = d3.scale.category20c();
-
-				_this.evtlineG.append('g').classed('event-group', true).selectAll('circles').data(_this._eventSerial(rows)).enter().append('circle').classed('event-node', true).attr({
-					cx: function cx(d, i) {
-						return _this.timeScale(d.dateObj.getTime());
-					},
-					cy: 100,
-					fill: function fill(d, i) {
-						return colorScale(i);
-					},
-					r: 12
-				}).call(function (circles) {
-					_this.eventNodes = circles;
-					_this.connectCircles(circles);
-				});
+				_this.initialize(_this._calSvgWidth(_this._eventSerial(rows))).drawChart(rows);
 			});
 
 			return this;
 		}
+
+		// Compress the data which has the same date.
+
 	}, {
-		key: 'connectCircles',
-		value: function connectCircles(circles) {
+		key: '_calSvgWidth',
+		value: function _calSvgWidth(data) {
+
+			var last = data.length - 1;
+
+			/*
+   	Calculate the weight of each date.
+   	For instance, if there are two events on the data.
+   	The weight of date is 2.
+   */
+			this.timeWeight = function (data) {
+
+				var _ = [],
+				    c = 0;
+
+				var n = 0,
+				    // The index of the data element
+				w = 1; // The weight of date
+
+				var _iteratorNormalCompletion = true;
+				var _didIteratorError = false;
+				var _iteratorError = undefined;
+
+				try {
+					for (var _iterator = data[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+						var d = _step.value;
+
+						// Check the date change.
+						// Once the date change, push the weight value to array
+						if (n && d.dateObj.getDate() !== data[n - 1].dateObj.getDate()) {
+							_.push(w);
+							w = 1;
+
+							// When the element is the last, push it anyway.
+							if (n === data.length - 1) _.push(w);
+						}
+
+						// Increment the weight of the date.
+						else if (n) w++;
+
+						n++;
+					}
+				} catch (err) {
+					_didIteratorError = true;
+					_iteratorError = err;
+				} finally {
+					try {
+						if (!_iteratorNormalCompletion && _iterator.return) {
+							_iterator.return();
+						}
+					} finally {
+						if (_didIteratorError) {
+							throw _iteratorError;
+						}
+					}
+				}
+
+				return _;
+			}(data);
+
+			// The period of time that the event starts to now (or to end.)
+			var period = Math.abs(data[last].dateObj - data[0].dateObj) / (24 * 60 * 60 * 1000);
+
+			return 250 * this.timeWeight.reduce(function (p, c) {
+				// Reduce the array into a weight summary value.
+				return p + c;
+			}) + (period - this.timeWeight.length) * this.timeSpace // Time slots for the dates without events.
+			;
+		}
+	}, {
+		key: 'drawChart',
+		value: function drawChart(data) {
 			var _this2 = this;
+
+			this._markCircles(data).call(function (circles) {
+				_this2._drawPath(circles).call(function (circles) {
+
+					// Mark the events on the line.
+					_this2._markEvts(circles);
+				});
+			});
+		}
+	}, {
+		key: '_markCircles',
+		value: function _markCircles(data) {
+			var _this3 = this;
+
+			var colorScale = d3.scale.category20c();
+
+			this._calY();
+
+			return this.evtlineG.append('g').classed('event-group', true).selectAll('circles').data(this.evtsData).enter().append('circle').classed('event-node', true).attr({
+				cx: function cx(d, i) {
+
+					var pDiff = 0,
+					    // the pixels length between two event.
+					diffDays = 0; // The diff of days.
+
+					for (var k = 0; k <= i; k++) {
+
+						// Calculate the diff days from the time starts
+						var _d = k > 0 ? Math.abs(_this3.evtsData[k].dateObj - _this3.evtsData[k - 1].dateObj) / (24 * 60 * 60 * 1000) - 1 : 0;
+						diffDays += _d;
+					}
+
+					// The serial time is not count
+					if (diffDays >= 2) pDiff = _this3.timeSpace * diffDays;
+
+					return 125 + 250 * i + pDiff;
+				},
+				// cy: 50,
+				cy: this.evtLineY,
+				fill: function fill(d, i) {
+					return colorScale(i);
+				},
+				r: 12
+			});
+		}
+	}, {
+		key: '_calY',
+		value: function _calY() {
+
+			console.log(this.pad.style('height'));
+
+			this.evtLineY = parseFloat(this.pad.style('height').replace('px', '')) - 30;
+		}
+	}, {
+		key: '_drawPath',
+		value: function _drawPath(circles) {
+			var _this4 = this;
 
 			var cPoses = [];
 
@@ -67,70 +198,109 @@ var EventLine = function () {
 				return d.cy;
 			});
 
-			circles.each(function (d) {
-
+			return circles.each(function (d, i) {
 				cPoses.push({
-					cx: _this2.timeScale(d.dateObj.getTime()),
-					cy: 100
+					cx: parseInt(d3.select(circles[0][i]).attr('cx').replace('px', '')),
+					// cy: 50
+					cy: _this4.evtLineY
 				});
 			}).call(function (circles) {
-
-				_this2.evtlineG.select('g.event-group').insert('g', ':first-child').classed('path-group', true).append('path').attr('d', _this2.line(cPoses)).attr('stoke-width', '2').attr('stroke', '#000');
+				_this4.evtlineG.insert('g', ':first-child').classed('path-group', true).append('path').attr('d', _this4.line(cPoses)).attr('stoke-width', '2').attr('stroke', '#000');
 			});
-
-			return this;
 		}
+
+		// Mark the events below the lines
+
+	}, {
+		key: '_markEvts',
+		value: function _markEvts(circles) {
+			var _this5 = this;
+
+			var data = [];
+
+			circles.each(function (d, i) {
+
+				var c = d3.select(circles[0][i]);
+
+				d.x = c.attr('cx');
+				d.y = c.attr('cy');
+
+				data.push(d);
+			}).call(function () {
+
+				var pl = parseFloat(d3.select('svg').style('padding-left').replace('px', ''));
+
+				_this5.evtInfoBoard.selectAll('div').data(data).enter().append('div').classed('event-info', true).style({
+					left: function left(d, i) {
+						// return pl+125+(250 * i)+-100 + 'px'
+						return pl + parseFloat(d.x) + -100 + 'px';
+					}
+				}).html(function (d) {
+
+					var time = d['Time'],
+					    event = d['Event'];
+
+					return '<div class="date"><span>' + time + '</span></div>' + '<div class="context">' + event + '</div>';
+				});
+			});
+		}
+
+		// Transfer the event's time value to date object.
+
 	}, {
 		key: '_eventSerial',
 		value: function _eventSerial(data) {
 
-			var evts = [],
-			    l = data.length;
+			var l = data.length;
 
-			var _iteratorNormalCompletion = true;
-			var _didIteratorError = false;
-			var _iteratorError = undefined;
+			var _iteratorNormalCompletion2 = true;
+			var _didIteratorError2 = false;
+			var _iteratorError2 = undefined;
 
 			try {
-				for (var _iterator = data[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-					var d = _step.value;
+				for (var _iterator2 = data[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
+					var d = _step2.value;
 
 					var parsedTime = d['Time'].match(this.timeRegExp);
 
 					// Date(year, month, date)
 					d.dateObj = new Date(parsedTime[1], parsedTime[2] - 1, parsedTime[3]);
 
-					evts.push(d);
+					this.evtsData.push(d);
 				}
 
 				// Sort the event in ascending order.
 			} catch (err) {
-				_didIteratorError = true;
-				_iteratorError = err;
+				_didIteratorError2 = true;
+				_iteratorError2 = err;
 			} finally {
 				try {
-					if (!_iteratorNormalCompletion && _iterator.return) {
-						_iterator.return();
+					if (!_iteratorNormalCompletion2 && _iterator2.return) {
+						_iterator2.return();
 					}
 				} finally {
-					if (_didIteratorError) {
-						throw _iteratorError;
+					if (_didIteratorError2) {
+						throw _iteratorError2;
 					}
 				}
 			}
 
-			evts.sort(function (a, b) {
+			this.evtsData.sort(function (a, b) {
 				return a.dateObj.getTime() - b.dateObj.getTime();
 			});
 
-			this.timeScale = d3.scale.linear().domain([evts[0].dateObj.getTime(), evts[l - 1].dateObj.getTime()]).range([0,
-			// parseInt(d3.select('svg').style('width').replace('px', ''))
-			function () {
-				var w = parseInt(d3.select('svg').style('width').replace('px', ''));
-				return 0.95 * w;
+			this.timeScale = d3.scale.linear().domain([this.evtsData[0].dateObj.getTime(), this.evtsData[l - 1].dateObj.getTime()]).range([0, function () {
+				var svg = d3.select('svg'),
+				    w = parseInt(svg.style('width').replace('px', '')),
+				   
+				// Value of Padding right and left
+				pl = parseInt(svg.style('padding-left').replace('px', '')),
+				    pr = parseInt(svg.style('padding-right').replace('px', ''));
+
+				return w - pl - pr;
 			}()]);
 
-			return evts;
+			return this.evtsData;
 		}
 	}]);
 
--- components/eventline.js
+++ components/eventline.js
@@ -1,10 +1,22 @@
 class EventLine {
 
 	constructor() {
+
+		this.pad = d3.select('#TimeLineBoard');
+
+		this.evtsData = [];
 		this.evtlineG = null;
+		this.evtLineY = null; // The Y position of the event line.
 		this.eventNodes = null;
 
-		this.line = null;
+		// this.line = null;
+		this.evtInfoBoard = null;
+
+		// The date weight for putting date in position.
+		this.timeWeight = null;
+		this.timeSpace  = 5;
+
+		this.textPointer = null;
 
 		// Time scale
 		this.timeScale = null;
@@ -13,10 +25,17 @@ class EventLine {
 
 	}
 
-	initialize() {
+	initialize(svgWidth) {
 		this.evtlineG = 
-			d3.select('svg')
-				.append('g').attr('id', 'EvtLine');
+			// d3.select('svg')
+			this.pad			
+				.attr('width', svgWidth)
+				.append('g')
+					.attr('id', 'EvtLine');
+
+		this.evtInfoBoard = 
+			d3.select('#TimeLineInfo')
+				.style('width', svgWidth + 'px');
 
 		return this
 	}
@@ -24,37 +43,143 @@ class EventLine {
 	csvImport(path) {
 
 		d3.csv(path)
-			.row((d, i) => { if (i !== 0) return d })
+			.row((d, i) => { return d })
 			.get((error, rows) => {
+
+				this
+					.initialize(
+						this._calSvgWidth(
+							this._eventSerial(rows)))
+					.drawChart(rows);
 				
-				let colorScale = d3.scale.category20c();
-
-				this.evtlineG
-					.append('g').classed('event-group', true)
-						.selectAll('circles')
-							.data(this._eventSerial(rows))
-								.enter().append('circle')
-									.classed('event-node', true)
-								.attr({
-									cx: (d, i) => { 
-										return this.timeScale(d.dateObj.getTime()) 
-									},
-									cy: 100,
-									fill: (d, i) => { return colorScale(i) },
-									r : 12
-								})
-								.call((circles) => {
-									this.eventNodes = circles;
-									this.connectCircles(circles);
-								});
-
 			});
 
 		return this
 	}
 
+	// Compress the data which has the same date.
+	_calSvgWidth(data) {
 
-	connectCircles(circles) {
+		let last = data.length - 1;
+
+		/*
+			Calculate the weight of each date.
+			For instance, if there are two events on the data.
+			The weight of date is 2.
+		*/
+		this.timeWeight = ((data) => {
+
+			let _ = [],
+				c = 0 ;
+
+			let n = 0, // The index of the data element
+				w = 1; // The weight of date
+
+			for (let d of data) {
+
+				// Check the date change. 
+				// Once the date change, push the weight value to array
+				if (n && (d.dateObj.getDate() !== data[n-1].dateObj.getDate())) {
+					_.push(w);
+					w = 1;
+
+					// When the element is the last, push it anyway.
+					if (n === data.length-1) _.push(w);
+				} 
+
+				// Increment the weight of the date.
+				else if (n) w++;
+
+				n++;
+			}
+			return _;
+		})(data);
+
+
+		// The period of time that the event starts to now (or to end.)
+		let period = 
+			Math
+				.abs(data[last].dateObj - data[0].dateObj) /
+					( 24 * 60 * 60 * 1000 );
+		
+		return (
+
+			250 * this.timeWeight
+				.reduce((p, c) => { // Reduce the array into a weight summary value.
+				return p+c
+				}) + 
+				( period -  this.timeWeight.length ) * this.timeSpace // Time slots for the dates without events. 
+		)
+			
+	}
+
+	drawChart(data) {
+
+		this._markCircles(data)
+			.call((circles) => {
+				this._drawPath(circles)
+					.call((circles) => {
+
+						// Mark the events on the line.
+						this._markEvts(circles);	
+						
+					});
+			});
+	}
+
+	_markCircles(data) {
+
+		let colorScale = d3.scale.category20c();
+
+		this._calY();
+		
+		return (
+			this.evtlineG
+			.append('g').classed('event-group', true)
+				.selectAll('circles')
+						.data(this.evtsData)
+						.enter().append('circle')
+							.classed('event-node', true)
+							.attr({
+								cx: (d, i) => { 
+									
+									let pDiff = 0,    // the pixels length between two event. 
+										diffDays = 0; // The diff of days.
+
+									for (let k = 0 ; k <= i; k ++) {
+
+										// Calculate the diff days from the time starts
+										let d = 
+											k > 0 ?
+												Math.abs(
+													this.evtsData[k].dateObj - this.evtsData[k-1].dateObj) / 
+										 				( 24 * 60 * 60 * 1000 ) - 1 : 0;
+										diffDays += d;
+									}
+
+									// The serial time is not count
+									if (diffDays >= 2) 
+										pDiff = this.timeSpace * diffDays;
+
+									return 125 + 250 * i + pDiff
+								},
+								// cy: 50,
+								cy: this.evtLineY,
+								fill: (d, i) => { return colorScale(i) },
+								r : 12
+							})
+			)
+	}
+
+	_calY() {
+
+		console.log(this.pad.style('height'));
+
+		this.evtLineY =  
+			parseFloat(this.pad.style('height').replace('px', '')) - 30;
+	}
+
+	_drawPath(circles) {
 
 		let cPoses = [];
 
@@ -63,34 +188,75 @@ class EventLine {
 				.x((d)=>{ return d.cx })
 				.y((d)=>{ return d.cy });
 
-		circles
-			.each((d) => {
-				
-				cPoses.push({
-					cx: this.timeScale(d.dateObj.getTime()) , 
-					cy: 100
-				});
-			})
-			.call((circles) => {
-				
-				this.evtlineG
-					.select('g.event-group')
+		return (
+			circles
+				.each((d, i) => {
+					cPoses.push({
+						cx: parseInt(d3.select(circles[0][i]).attr('cx').replace('px', '')),
+						// cy: 50
+						cy: this.evtLineY
+					});
+				})
+				.call((circles) => {
+					this.evtlineG
 						.insert('g', ':first-child')
-						.classed('path-group', true)
-							.append('path')
-								.attr('d', this.line(cPoses))
-								.attr('stoke-width', '2')
-								.attr('stroke', '#000');
-			});
+							.classed('path-group', true)
+								.append('path')
+									.attr('d', this.line(cPoses))
+									.attr('stoke-width', '2')
+									.attr('stroke', '#000');
+				})
+		)
+	}
 
-		return this
+	// Mark the events below the lines
+	_markEvts(circles) {
+
+		let data = [];
+
+		circles.each((d, i) => {
+
+			let c = d3.select(circles[0][i]);
+
+			d.x = c.attr('cx');
+			d.y = c.attr('cy');
+			
+			data.push(d);
+		})
+		.call(() => {
+
+			let pl = 
+				parseFloat(
+					d3.select('svg').style('padding-left').replace('px', ''));
+
+			this.evtInfoBoard.selectAll('div')
+				.data(data).enter()
+					.append('div')
+						.classed('event-info', true)
+							.style({
+								left: (d, i) => { 
+									// return pl+125+(250 * i)+-100 + 'px' 
+									return pl+parseFloat(d.x)+(-100) + 'px'
+								}
+							})
+							.html((d) => {
+
+								let time = d['Time'],
+									event = d['Event'];
+
+								return (
+									'<div class="date"><span>' + time + '</span></div>' +
+									'<div class="context">'+ event + '</div>'
+									)
+							});
+		});
 
 	}
 
+	// Transfer the event's time value to date object.
 	_eventSerial(data) {
 
-		let evts = [],
-			l    = data.length;
+		let l  = data.length;
 		
 		for (let d of data) {
 			let parsedTime = d['Time'].match(this.timeRegExp);
@@ -99,30 +265,35 @@ class EventLine {
 				d.dateObj = 
 					new Date(parsedTime[1], parsedTime[2]-1, parsedTime[3]);
 
-			evts.push(d);
+			this.evtsData.push(d);
 		}
 
+
 		// Sort the event in ascending order.
-		evts.sort((a, b) => {
+		this.evtsData.sort((a, b) => {
 			return a.dateObj.getTime() - b.dateObj.getTime()
 		});
-
 
 		this.timeScale = 
 			d3.scale.linear()
 				.domain([
-					evts[0].dateObj.getTime(),
-					evts[l-1].dateObj.getTime()
+					this.evtsData[0].dateObj.getTime(),
+					this.evtsData[l-1].dateObj.getTime()
 				])
 				.range([
 					0,
 					(() => {
-						let w = parseInt(d3.select('svg').style('width').replace('px', ''));
-						return 0.95*w
+						let svg = d3.select('svg'),
+							w = parseInt(svg.style('width').replace('px', '')),
+							// Value of Padding right and left
+							pl = parseInt(svg.style('padding-left').replace('px', '')),
+							pr = parseInt(svg.style('padding-right').replace('px', ''));
+						
+						return w-pl-pr
 					})()
 				]);
-
-		return evts
+		
+		return this.evtsData
 
 	}
 
