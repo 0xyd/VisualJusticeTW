477fb64016ee5693ec32a1dc0287030b7c699590
YuDe <davisfreeman1015@gmail.com>
2016-12-11 20:51:37 +0800

    Adjust a little bit code.

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  entry: path.join(__dirname, 'scripts', 'jrcf.js'),
+  output: {
+    path: path.join(__dirname),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: path.join(__dirname, 'components'),
+        loader: ['babel-loader'],
+        query: {
+          cacheDirectory: 'babel_cache',
+          presets: ['react', 'es2015']
+        }
+      },
+      {
+        test: path.join(__dirname, 'scripts', 'jrcf.js'),
+        loader: ['babel-loader'],
+        query: {
+          cacheDirectory: 'babel_cache',
+          presets: ['react', 'es2015']
+        }
+      }
+      
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    }),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: { warnings: false },
+      mangle: true,
+      sourcemap: false,
+      beautify: false,
+      dead_code: true
+    })
+  ]
+}
